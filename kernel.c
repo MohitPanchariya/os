9e1d8e96e20a69fa7bcae9e44cf36bdb03759229
Mohit Panchariya <iammohitpanchariya@gmail.com>
2026-07-30 21:16:32 +0530

    Implement minimal kernel
    This commit contains a kernel that spins forever,
    along with a linker script, and a script to run
    the kernel on qemu.

---
+++ kernel.c
@@ -0,0 +1,20 @@
+extern char __stack_top[];
+
+void kmain(void) {
+    for (;;); // spin forever
+}
+
+/*
+The boot function is placed at .text.boot since
+the first section in the linked executable is .text.boot
+*/
+__attribute__((section(".text.boot")))
+__attribute__((naked))
+void boot(void) {
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n" // place stack top in stack pointer
+        "j kmain\n" // jump to kmain function
+        :
+        : [stack_top] "r" (__stack_top)
+    );
+}
