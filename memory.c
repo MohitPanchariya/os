9d304183646e0e91126fb4fa25f04eac05d78064
Mohit Panchariya <iammohitpanchariya@gmail.com>
2026-08-01 17:01:06 +0530

    Implement memory operations

---
+++ memory.c
@@ -0,0 +1,19 @@
+#include "memory.h"
+
+void* memset(void* buf, char fill, size_t buf_size) {
+    uint8_t* p = (uint8_t* ) buf;
+    while(buf_size--) {
+        *p++ = fill;
+    }
+    return buf;
+}
+
+void* memcpy(void* dst, const void* src, size_t n) {
+    uint8_t* d = (uint8_t*) dst;
+    const uint8_t* s = (const uint8_t*) src;
+
+    while(n--) {
+        *d++ = *s++;
+    }
+    return dst;
+}
--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ set -xue
 CC=/opt/homebrew/opt/llvm/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
-$CC $CFLAGS -Wl,-TKernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c io.c sbi.c
+$CC $CFLAGS -Wl,-TKernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c io.c sbi.c memory.c
 
 qemu-system-riscv32 -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
